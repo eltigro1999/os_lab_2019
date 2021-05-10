85c59a997367834dd9b6173e0b935b14dc085c8a
eltigro1999 <eltigrolearning@gmail.com>
2021-05-10 03:32:25 +0300

    Lab 5 done

---
+++ lab5/src/deadlock.c
@@ -0,0 +1,72 @@
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+pthread_mutex_t lock1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
+
+void do_one_thing(){
+
+    pthread_mutex_lock(&lock1);
+    printf("took the bow\n");
+    
+
+    sleep(1);
+    printf("trying to take arrows\n");
+
+    pthread_mutex_lock(&lock2); 
+    
+    pthread_mutex_unlock(&lock2);
+
+    pthread_mutex_unlock(&lock1);
+
+    pthread_exit(NULL);
+
+}
+
+void do_another_thing(){
+
+    pthread_mutex_lock(&lock2);
+    printf("took arrows\n");
+    
+
+    sleep(1);
+    printf("trying to take a bow\n");
+
+    pthread_mutex_lock(&lock1); 
+
+
+    pthread_mutex_unlock(&lock1);
+
+    pthread_mutex_unlock(&lock2);
+
+    pthread_exit(NULL);
+
+}
+
+int main() {
+  pthread_t thread1, thread2;
+
+  if (pthread_create(&thread1, NULL, (void *)do_one_thing,NULL)!= 0) {
+    perror("pthread_create");
+    exit(1);
+  }
+
+  if (pthread_create(&thread2, NULL, (void *)do_another_thing, NULL) != 0) {
+    perror("pthread_create");
+    exit(1);
+  }
+
+  if (pthread_join(thread1, NULL) != 0) {
+    perror("pthread_join");
+    exit(1);
+  }
+
+  if (pthread_join(thread2, NULL) != 0) {
+    perror("pthread_join");
+    exit(1);
+  }
+
+  return 0;
+}
